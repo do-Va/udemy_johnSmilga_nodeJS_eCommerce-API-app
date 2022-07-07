2472e3d8c60e154f27a86ba18b053833231d200d
do-Va <varisdogukan@outlook.com>
2022-07-07 14:58:17 +0300

    Basic Routes and Middleware

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
+require('dotenv').config();
+require('express-async-errors');
+
+// express
 const express = require('express');
 const app = express();
-require('dotenv').config();
 
 // database
 const connectDB = require('./db/connect');
 
+//middleware
+const notfoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+// req.body içerisindeki json verileri almamızı sağlıyor.
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('hello');
+  res.send('<h1 >e-commerce api</h1>');
 });
+
+app.use(notfoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 const start = async () => {
